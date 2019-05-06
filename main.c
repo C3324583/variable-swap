c1a1772ded0ae7023ca3dabcb37abcd356c77914
C3324583 <48045839+C3324583@users.noreply.github.com>
2019-05-06 14:57:22 +1000

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+void swap(int *t, int*s);
+
+int main()
+{
+    int x=1, y=2;
+    int *a, *b;
+
+    a = &x;
+    b = &y;
+    
+    printf ("%d  %d\n", x, y);
+    swap(a,b);
+    printf("%d  %d\n", x, y);
+    
+    return 0;
+}
+
+
+void swap(int *t, int*s){
+    int temp;
+    temp = *t;
+    *t = *s;
+    *s = temp;
+}
